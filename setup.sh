b17b4df31cab4bec147450d35c1c92332820e5f6
troy600 <bazartroy531@gmail.com>
2025-01-18 16:11:45 +0800

    beta release

--- setup.sh
+++ setup.sh
@@ -3,6 +3,24 @@
 
 linux_version="6.12.10"
 
-cd "linux-$linux_version"
+cd "linux-$linux_version" || exit
+cp ../PKGBUILD ./PKGBUILD
 
-cp ../PKGBUILD ./
+makeconfig() {
+    make menuconfig -j$(nproc)
+}
+
+makeconfig
+if [[ ! -f ".config" ]]; then
+    echo "no config detected. Possible that the user didn't save his .config file"
+    return 1
+fi
+
+installkernel() {
+    sudo pacman -U linux-upstream-6* linux-upstream-api-headers-6*
+}
+
+build() {
+    make -j$(nproc)
+}
+
