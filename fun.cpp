57b4b90736c0d488c96fbdeffc3870d19316fe73
troy600 <bazartroy531@gmail.com>
2025-01-18 17:37:45 +0800

    also this

---
+++ fun.cpp
@@ -0,0 +1,23 @@
+#define uwu <iostream>
+#define uwo std
+#define owo main
+#define OwO cout 
+#define UwU return
+#define UWU 0
+#define uWu "hello world"
+#define oWo <<
+#define OWO int
+#define _w_ (
+#define oWO )
+#define uWo {
+#define owu }
+#define OWU ;
+#define Uwu ::
+
+#include uwu
+
+OWO owo _w_ oWO uWo
+    uwo Uwu OwO oWo uWu OWU
+    UwU UWU OWU
+owu
+
