d7db94ed14891e18198bb757e99e1d292d3e6fcc
troy600 <bazartroy531@gmail.com>
2025-01-18 16:37:53 +0800

    linux 6.12.10 update

--- setup.sh
+++ setup.sh
@@ -3,24 +3,41 @@
 
 linux_version="6.12.10"
 
-cd "linux-$linux_version" || exit
-cp ../PKGBUILD ./PKGBUILD
+
+starting() {
+    cd "linux-$linux_version" || exit
+    rm -rvf linux-upstream-6* *headers*
+    #cp ../PKGBUILD ./PKGBUILD
+}
+downloadkernel() {
+    if [[ -f "linux-6.12.10/*" ]]; then 
+        rsync rsync://rsync.kernel.org/pub/linux/kernel/v6.x/linux-6.12.10.tar.xz . -rvz
+    else
+        echo "nothing"
+    fi
+}
 
 makeconfig() {
     make menuconfig -j$(nproc)
 }
 
-makeconfig
-if [[ ! -f ".config" ]]; then
-    echo "no config detected. Possible that the user didn't save his .config file"
-    return 1
-fi
-
 installkernel() {
-    sudo pacman -U linux-upstream-6* linux-upstream-api-headers-6*
+    sudo pacman -U linux-upstream-6* *headers*
 }
 
 build() {
-    make -j$(nproc)
+    if [[ ! -f ".config" ]]; then
+        echo "no config detected. Possible that the user didn't save his .config file"
+    fi
+
+    make -j$(nproc) pacman-pkg
 }
 
+
+
+
+downloadkernel
+starting
+makeconfig
+build
+installkernel
