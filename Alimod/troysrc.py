f2787dd44b7e7ff08879a78c54e80c3f86b7fffa
troy600 <bazartroy531@gmail.com>
2025-01-19 20:03:31 +0800

    more functions

--- Alimod/troysrc.py
+++ Alimod/troysrc.py
@@ -1,22 +1,18 @@
-import asyncio
 import os
-import subprocess
 
-async def bash(command):
-    process = await asyncio.create_subprocess_shell(
-        command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    if stdout:
-        print(f"{stdout.decode()}")
-        return 0
-    if stderr:
-        print(f"{stderr.decode()}")
-        return 1
+def MenuconfigPacman():
+    os.system("cd linux-6.12.10 && make menuconfig -j4")
+    os.system(f"cd linux-6.12.10 && make -j {os.cpu_count()} pacman-pkg")
 
-def menuconfig():
-    syn = os.system("cd linux-6.12.10 && make menuconfig -j4")
-    nproc = asyncio.run(bash("nproc"))
-    os.system(f"cd linux-6.12.10 && make -j {os.cpu_count()}")
+#don't mind this
+def MenuconfigDebian():
+    os.system("cd linux-6.12.10 && make menuconfig -j4")
+    os.system(f"cd linux-6.12.10 && make -j {os.cpu_count()} deb-pkg")
+
+def DefconfigPacman():
+    os.system("cd linux-6.12.10 && make defconfig -j4")
+    os.system(f"cd linux-6.12.10 && make -j {os.cpu_count()} pacman-pkg")
+
+def LocalModConfigpacman():
+    os.system("cd linux-6.12.10 && make localmodconfig")
+    os.system(f"cd linux-6.12.10 && make -j {os.cpu_count()} pacman-pkg")
