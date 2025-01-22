c556f157516cb4cae55292be81c9140f27fbfcbc
troy600 <bazartroy531@gmail.com>
2025-01-22 20:13:51 +0800

    Official 0.1-rc1

--- Alimod/versions.py
+++ Alimod/versions.py
@@ -2,21 +2,20 @@ import os
 
 
 def linux613():
-    os.system("wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.13.tar.xz'")
-    os.system("tar -xvf linux-6.13.tar.xz")
+    linuxversion = "linux-6.13"
+    if os.path.exists(linuxversion):
+        os.system("wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.13.tar.xz'")
+        os.system("tar -xvf linux-6.13.tar.xz")
 
-    return "linux-6.13"
+    return linuxversion
 
 
 def linux612():
-    os.system("wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.10.tar.xz'")
-    return "linux-6.12.10"
+    linuxversion = "linux-6.12.10"
+    if os.path.exists(linuxversion) == False:
+        os.system("wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.10.tar.xz'")
+        os.system(f"tar -xvf {linuxversion}.tar.xz")
+    return linuxversion
 
 def linux66():
     return "linux-6.6.73"
-
-def linux61():
-    return "linux-6.1.126"
-
-def linux515():
-    return "linux-5.15.176"
