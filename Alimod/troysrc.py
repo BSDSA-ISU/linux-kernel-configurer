bcac52ec8885c8e5dafb0c324bc4be372fc0e721
troy600 <bazartroy531@gmail.com>
2025-01-22 20:13:32 +0800

    Official 0.1-rc1

--- Alimod/troysrc.py
+++ Alimod/troysrc.py
@@ -14,6 +14,8 @@ def MenuconfigDebian(version):
 def DefconfigPacman(version):
     os.system(f"zcat /proc/config.gz > {version}/.config")
     os.system(f"cd {version} && make oldconfig -j4")
+    print("\n\nfinalize it\n\n")
+    os.system(f"cd {version} && make menuconfig -j4")
     os.system(f"cd {version} && make -j {os.cpu_count()} pacman-pkg")
     print("it may ask you that linux-upstream-api-headres and linux-api-headers are in conflict just click y no need 2 worry")
     os.system(f"cd {version} && sudo pacman -U *headers* *linux-upstream-6*")
