db5bb72a0b32f3aa63660ebb745c74b1d6eb558a
troy600 <bazartroy531@gmail.com>
2025-01-24 16:27:15 +0800

    change version to latest

--- Alimod/versions.py
+++ Alimod/versions.py
@@ -4,18 +4,26 @@ import os
 def linux613():
     linuxversion = "linux-6.13"
     if os.path.exists(linuxversion):
-        os.system("wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.13.tar.xz'")
-        os.system("tar -xvf linux-6.13.tar.xz")
+        os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
+        os.system(f"tar -xvf {linuxversion}.tar.xz")
 
     return linuxversion
 
 
 def linux612():
-    linuxversion = "linux-6.12.10"
+    linuxversion = "linux-6.12.11"
     if os.path.exists(linuxversion) == False:
-        os.system("wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.10.tar.xz'")
+        os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
         os.system(f"tar -xvf {linuxversion}.tar.xz")
     return linuxversion
 
 def linux66():
-    return "linux-6.6.73"
+    linuxversion = "linux-6.6.74"
+    if os.path.exists(linuxversion):
+        os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
+        os.system(f"tar -xvf {linuxversion}.tar.xz")
+    return linuxversion
+
+def linux61():
+    linuxversion = "linux-6.1.127"
+    return "linux-6.1.127"
