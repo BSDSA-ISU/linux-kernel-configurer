a854c5e68876b99f707f4619140962d49e5836ad
troy600 <bazartroy531@gmail.com>
2025-02-11 11:24:51 +0800

    Version linux 6.14 rc2 added

--- Alimod/versions.py
+++ Alimod/versions.py
@@ -1,12 +1,18 @@
 import os
 
+class myclass:
+    def myself():
+        print()
+
+myclass.myself()
+
 def linuxrc():
     rclink = "https://git.kernel.org/torvalds/t/linux-6.14-rc1.tar.gz"
-    if os.path.exists("linux-6.14-rc1.tar.gz") == False:
+    if os.path.exists("linux-6.14-rc2.tar.gz") == False:
         os.system(f"wget {rclink}")
 
 def linux613():
-    linuxversion = "linux-6.13.1"
+    linuxversion = "linux-6.13.2"
     if os.path.exists(linuxversion) == False:
         os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
         os.system(f"tar -xvf {linuxversion}.tar.xz")
@@ -15,14 +21,14 @@ def linux613():
 
 
 def linux612():
-    linuxversion = "linux-6.12.12"
+    linuxversion = "linux-6.12.13"
     if os.path.exists(linuxversion) == False:
         os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
         os.system(f"tar -xvf {linuxversion}.tar.xz")
     return linuxversion
 
 def linux66():
-    linuxversion = "linux-6.6.75"
+    linuxversion = "linux-6.6.76"
     if os.path.exists(linuxversion) == False:
         os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
         os.system(f"tar -xvf {linuxversion}.tar.xz")
