fd4efc41f17b9a885f1e724511c7a3a6eb1a3f2e
troy600 <bazartroy531@gmail.com>
2025-02-24 19:58:25 +0800

    Rc4 release

--- Alimod/versions.py
+++ Alimod/versions.py
@@ -10,7 +10,7 @@ myclass.myself()
 class Version:
     @staticmethod
     def linuxrc():
-        rclink = "https://git.kernel.org/torvalds/t/linux-6.14-rc3.tar.gz"
+        rclink = "https://git.kernel.org/torvalds/t/linux-6.14-rc4.tar.gz"
         if os.path.exists("linux-6.14-rc2.tar.gz") == False:
             os.system(f"wget {rclink}")
 
@@ -46,3 +46,13 @@ class Version:
             os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
             os.system(f"tar -xvf {linuxversion}.tar.xz")
         return linuxversion
+
+    @staticmethod
+    def linux515():
+        linuxversion = "linux-5.15.178"
+        if os.path.exists(linuxversion) == False:
+            os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
+            os.system(f"tar -xvf {linuxversion}.tar.xz")
+        return linuxversion
+
+
