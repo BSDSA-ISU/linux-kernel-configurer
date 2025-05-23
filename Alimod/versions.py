289971a1b619feac2c64900bd7c13a630d11ef4b
troy600 <bazartroy531@gmail.com>
2025-05-23 10:02:37 +0800

    S

--- Alimod/versions.py
+++ Alimod/versions.py
@@ -10,7 +10,7 @@ myclass.myself()
 class Version:
     @staticmethod
     def linuxrc():
-        linuxversion = "linux-6.15-rc6"
+        linuxversion = "linux-6.15-rc7"
         rclink = "https://git.kernel.org/torvalds/t/linux-6.15-rc6.tar.gz"
         if os.path.exists("linux-6.15-rc6.tar.gz") == False:
             os.system(f"wget {rclink}")
@@ -19,7 +19,7 @@ class Version:
 
     @staticmethod
     def linux613():
-        linuxversion = "linux-6.14.6"
+        linuxversion = "linux-6.14.8"
         if os.path.exists(linuxversion) == False:
             os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
             os.system(f"tar -xvf {linuxversion}.tar.xz")
@@ -28,7 +28,7 @@ class Version:
 
     @staticmethod
     def linux612():
-        linuxversion = "linux-6.12.28"
+        linuxversion = "linux-6.12.30"
         if os.path.exists(linuxversion) == False:
             os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
             os.system(f"tar -xvf {linuxversion}.tar.xz")
