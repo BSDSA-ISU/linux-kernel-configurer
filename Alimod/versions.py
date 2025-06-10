e2e7e0604636d9d1288ad2579a5ae47e3f895584
troy600 <bazartroy531@gmail.com>
2025-06-10 23:03:04 +0800

    revise

--- Alimod/versions.py
+++ Alimod/versions.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 class myclass:
     @staticmethod
@@ -19,8 +20,7 @@ class Version:
         return linuxversion
 
     @staticmethod
-    def linux613():
-        linuxversion = "linux-6.14.10"
+    def linuxdownload(linuxversion):
         if os.path.exists(linuxversion) == False:
             os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
             os.system(f"tar -xvf {linuxversion}.tar.xz")
@@ -34,6 +34,30 @@ class Version:
             os.system(f"wget 'https://cdn.kernel.org/pub/linux/kernel/v6.x/{linuxversion}.tar.xz'")
             os.system(f"tar -xvf {linuxversion}.tar.xz")
         return linuxversion
+
+    @staticmethod
+    def getver(numbr):
+        url = f"https://raw.githubusercontent.com/AlieeLinux/linux-6.12.X/refs/heads/main/versions/{numbr}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            version = response.text.strip()
+            return version
+        else:
+            print("no version available. exiting...")
+            exit(9)
+    
+    def fetchver(self, kerneltype):
+        if kerneltype == "12lts":
+            ver = "linux612lts"
+        elif kerneltype == "stable":
+            ver = "linuxstable"
+
+        version = Version.getver(ver)
+        Version.linuxdownload(version)
+
+
+    
 '''
     @staticmethod
     def linux66():
