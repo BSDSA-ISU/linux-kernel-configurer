59a33ca0c71c465d9dd8dbe721870b40c07bcc56
AlieeLinux <bazartroy531@gmail.com>
2025-09-04 19:39:56 +0800

    morbidus

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-from Alimod import clean
+import requests
 
-clean.clean()
+url = "https://raw.githubusercontent.com/AlieeLinux/linux-6.12.X/refs/heads/main/versions/linux612"
+response = requests.get(url)
+
+if response.status_code == 200:
+    version = response.text.strip()
+    print("Version:", version)
+else:
+    print("Failed to retrieve the file.")
